3aabc29f486f37fadef996f9ff6933ee09520783
Benjamón Chávez <bhcg20041989@gmail.com>
2021-08-12 11:45:26 -0500

    Iniciando commits con algo de avance en el site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+const { Stack, Text, Image } = require("@chakra-ui/react");
 
 function App() {
+  const [gifs,setGifs]=useState(['https://media.giphy.com/media/fAmnJKCwuXtDiEhNwg/giphy.gif'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack>
+        <Text>Hola Mundo con chakra</Text>
+        <Image w="200px" src={gifs}></Image>
+      </Stack>
     </div>
   );
 }
