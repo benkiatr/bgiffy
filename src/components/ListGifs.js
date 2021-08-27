d42b3395120bc56ebbb6a0a4beec0654ff7afc60
Benjamón Chávez <bhcg20041989@gmail.com>
2021-08-27 09:10:09 -0500

    corrigiendo detalles de estilo y fucnionalidad

--- src/components/ListGifs.js
+++ src/components/ListGifs.js
@@ -1,4 +1,5 @@
 import React,{ useState, useEffect} from "react";
+import { SimpleGrid } from "@chakra-ui/react"
 import getGifs from '../apis/getGifs'
 import Gif from "./Gif";
 
@@ -12,17 +13,17 @@ export default function ListGifs({params}) {
   }, [keysearch]);
 
   return (
-    <>
-    {
-      gifs.map(({ id, url, title }) => (
-        <Gif 
-          key={id} 
-          id={id} 
-          title={title} 
-          url={url} 
-          />
-      ))
-    }
-    </>
+    <SimpleGrid columns={{ sm:2,md:4,lg:6 }} spacing={2}>
+      {
+        gifs.map(({ id, url, title }) => (
+          <Gif 
+            key={id} 
+            id={id} 
+            title={title} 
+            url={url} 
+            />
+        ))
+      }
+    </SimpleGrid>
   )
 }
